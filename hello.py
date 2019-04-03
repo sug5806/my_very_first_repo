2dbf58ae44de79c028589dddee4e2a5469c8ab43
sug5806 <sug5806@gmail.com>
2019-04-03 11:24:09 +0900

    feat: edit hello.py
    I add condtional statement to print 'Hello World' when i is even.

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-for _ in range(5):
-    print("Hello World!")
+# "hello world"를 출력하시오.
+# "hello world"를 5번 출력하시오.
+# "hello world"를 내부 변수 i가 짝수일때만 출력하시오.(i는 1~11)
+
+
+for i in range(1, 11):
+    if i % 2 == 0:
+    	print("Hello World!")
